1bda1941c0de0e00bbdea6aaffd440c2b019d7ce
christianacca <christian.crowhurst@gmail.com>
2018-09-18 09:44:03 +0100

    feat(AppStoppedLogEventInfo): new event

---
+++ src/CcAcca.LogEvents.Tests/AppStoppedLogEventInfoSpecs.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace CcAcca.LogEvents.Tests
+{
+    [TestFixture]
+    public class AppStoppedLogEventInfoSpecs
+    {
+        [Test]
+        public void SignalTime_supplied_in_Ctor()
+        {
+            var signalTime = DateTime.Now.AddMilliseconds(-200);
+
+            var evt = new AppStoppedLogEventInfo(signalTime);
+
+            Assert.That(evt.ShutdownDuration.TotalMilliseconds, Is.EqualTo(200d).Within(3));
+        }
+
+        [Test]
+        public void No_SignalTime_supplied_in_Ctor()
+        {
+            var evt = new AppStoppedLogEventInfo();
+
+            Assert.That(evt.ShutdownDuration, Is.EqualTo(TimeSpan.Zero));
+        }
+
+        [Test]
+        public void AppStarted_supplied_in_Ctor()
+        {
+            var appStarted = DateTime.Now.AddMilliseconds(-200);
+
+            var evt = new AppStoppedLogEventInfo(appStarted: appStarted);
+
+            Assert.That(evt.TotalRuntime.TotalMilliseconds, Is.EqualTo(200d).Within(3));
+        }
+
+        [Test]
+        public void No_AppStarted_supplied_in_Ctor()
+        {
+            var evt = new AppStoppedLogEventInfo();
+
+            Assert.That(evt.TotalRuntime, Is.EqualTo(TimeSpan.Zero));
+        }
+
+        [Test]
+        public void Should_use_prefix()
+        {
+            var evt = new AppStoppedLogEventInfo();
+
+            Assert.That(evt.Prefix, Is.EqualTo(LogPrefixes.AppEvent));
+        }
+    }
+}
---
+++ src/CcAcca.LogEvents/AppStoppedLogEventInfo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CcAcca.LogEvents
+{
+    public class AppStoppedLogEventInfo : LogEventInfo
+    {
+        private const string ShutdownMetricKey = "ShutdownMsec";
+        private const string TotalRuntimeMetricKey = "TotalRuntimeMsec";
+
+        public AppStoppedLogEventInfo(DateTime? signalTime = null, DateTime? appStarted = null) : base("Stopped", LogPrefixes.AppEvent)
+        {
+            if (signalTime.HasValue)
+            {
+                ShutdownDuration = DateTime.Now - signalTime.Value;
+            }
+            if (appStarted.HasValue)
+            {
+                TotalRuntime = DateTime.Now - appStarted.Value;
+            }
+        }
+
+        public TimeSpan ShutdownDuration
+        {
+            get => Metrics.ContainsKey(ShutdownMetricKey)
+                ? TimeSpan.FromMilliseconds(Metrics[ShutdownMetricKey])
+                : TimeSpan.Zero;
+            set => Metrics[ShutdownMetricKey] = Math.Round(value.TotalMilliseconds, 0);
+        }
+
+        public TimeSpan TotalRuntime
+        {
+            get => Metrics.ContainsKey(TotalRuntimeMetricKey)
+                ? TimeSpan.FromMilliseconds(Metrics[TotalRuntimeMetricKey])
+                : TimeSpan.Zero;
+            set => Metrics[TotalRuntimeMetricKey] = Math.Round(value.TotalMilliseconds, 0);
+        }
+    }
+}

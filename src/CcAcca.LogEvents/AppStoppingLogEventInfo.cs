0bcba3113e4b64141e41b8b2ec55b7f02741a715
christianacca <christian.crowhurst@gmail.com>
2018-09-18 09:43:34 +0100

    feat(AppStoppingLogEventInfo): new event

---
+++ src/CcAcca.LogEvents/AppStoppingLogEventInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace CcAcca.LogEvents
+{
+    public class AppStoppingLogEventInfo : LogEventInfo
+    {
+        public AppStoppingLogEventInfo() : base("Stopping", LogPrefixes.AppEvent)
+        {
+        }
+    }
+}

afbed4a279e0a98fc082daa8978409ef1178c54c
christianacca <christian.crowhurst@gmail.com>
2018-09-18 12:31:45 +0100

    refactor(LogEventInfo): move class into internal namespace

--- src/CcAcca.LogEvents/LogEventInfo.cs
+++ src/CcAcca.LogEvents/LogEventInfo.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using CcAcca.LogEvents.Internals;
 
 namespace CcAcca.LogEvents
@@ -94,66 +92,6 @@ namespace CcAcca.LogEvents
         {
             get => (_defaultSerializer = _defaultSerializer ?? new TextLogEventInfoSerializer());
             set => _defaultSerializer = value;
-        }
-
-        public class TextLogEventInfoSerializer : ILogEventInfoSerializer
-        {
-            private const string EventFieldName = "Event";
-            private const string FieldTemplate = "{0}: {1}";
-
-            public string TextLogFieldSeperator { get; set; } = " |";
-
-            public string Serialize(LogEventInfo source)
-            {
-                if (!source.HasMetric && !source.HasProperty)
-                {
-                    return string.Format(FieldTemplate, EventFieldName, source.FullName);
-                }
-
-                var properties = source.GetQualifiedProperties();
-                var metrics = source.GetQualifiedMetrics();
-                int elementCount = GetPropertyAndMetricCount(properties, metrics);
-                var sb = new StringBuilder(1 + elementCount * 50);
-                sb.AppendFormat(FieldTemplate, EventFieldName, source.FullName);
-                WriteCollection(properties, sb);
-                WriteCollection(metrics, sb);
-                return sb.ToString();
-            }
-
-            private void WriteCollection<T>(IDictionary<string, T> values, StringBuilder sb)
-            {
-                if (values == null || !values.Any()) return;
-
-                sb.Append(TextLogFieldSeperator);
-
-                int i = 1;
-                foreach (var entry in values)
-                {
-                    sb.AppendFormat(FieldTemplate, entry.Key, entry.Value);
-                    if (i < values.Count)
-                    {
-                        sb.Append(TextLogFieldSeperator);
-                    }
-
-                    i++;
-                }
-            }
-
-            private static int GetPropertyAndMetricCount(IDictionary<string, string> properties, IDictionary<string, double> metrics)
-            {
-                int size = 0;
-                if (properties != null)
-                {
-                    size += properties.Count;
-                }
-
-                if (metrics != null)
-                {
-                    size += metrics.Count;
-                }
-
-                return size;
-            }
         }
     }
 }
